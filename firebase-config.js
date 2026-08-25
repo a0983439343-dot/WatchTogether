1058b003f82c62e5ef729cf7bebe53aed8135555
a0983439343-dot <a0983439343@gmail.com>
2026-08-25 23:17:24 +0800

    Refactor Firebase config to window object
    Moved Firebase configuration to window.FIREBASE_CONFIG and removed initialization code.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
-
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDZntXy7hLNzBlADp94MyoRmiFWSSdvDLE",
   authDomain: "watchtogether-3f4f9.firebaseapp.com",
   databaseURL: "https://watchtogether-3f4f9-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -11,13 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "726694766811",
   appId: "1:726694766811:web:238f98e46330d0f65884fe"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getDatabase(app);
-
-export async function loginAnonymously() {
-  const result = await signInAnonymously(auth);
-  return result.user;
-}
