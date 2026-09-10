2c567cdcc522af77d92aa0aafc4020da6cbc2d2a
a0983439343-dot <a0983439343@gmail.com>
2026-09-10 22:56:17 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,10 @@
-// WatchTogether Firebase 設定
-// 請把下面內容替換成 Firebase Console → 專案設定 → Web App 的實際設定。
-// 這個檔案由 index.html 以一般 <script> 載入，因此不要使用 export。
-
-const firebaseConfig = {
+window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyDZntXy7hLNzBlADp94MyoRmiFWSSdvDLE",
   authDomain: "watchtogether-3f4f9.firebaseapp.com",
   databaseURL: "https://watchtogether-3f4f9-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "watchtogether-3f4f9",
   storageBucket: "watchtogether-3f4f9.firebasestorage.app",
   messagingSenderId: "726694766811",
-  appId: "1:726694766811:web:238f98e46330d0f65884fe"
+  appId: "1:726694766811:web:238f98e46330d0f65884fe",
+  measurementId: "G-VZXG2CRVE0"
 };
-
-window.FIREBASE_CONFIG = firebaseConfig;
