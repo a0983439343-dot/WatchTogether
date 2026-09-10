2f8e8b62197e753c24053d8d01569935d8a181f3
a0983439343-dot <a0983439343@gmail.com>
2026-09-10 22:28:17 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDZntXy7hLNzBlADp94MyoRmiFWSSdvDLE",
+  authDomain: "watchtogether-3f4f9.firebaseapp.com",
+  databaseURL: "https://watchtogether-3f4f9-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "watchtogether-3f4f9",
+  storageBucket: "watchtogether-3f4f9.firebasestorage.app",
+  messagingSenderId: "726694766811",
+  appId: "1:726694766811:web:238f98e46330d0f65884fe"
+};
