68c48d38350733b26bcc46c538d3b7163080f952
a0983439343-dot <a0983439343@gmail.com>
2026-09-25 22:39:50 +0800

    Clean up duplicate escape helper binding

--- src/js/enhancements.js
+++ src/js/enhancements.js
@@ -709,7 +709,6 @@ var openModal = wt.openModal;
 var closeModal = wt.closeModal;
 var serverTs = wt.serverTs;
 var FRIEND_CODE_RE = wt.FRIEND_CODE_RE;
-var esc = wt.esc;
 var isCurrentAuthUser = wt.isCurrentAuthUser;
 
 async function reservePublicId(code,user) {
